2743bce1c90d30e64bb284a77b690b60d593a139
Anirudh-J-8 <anirudhj.8@gmail.com>
2025-06-13 17:33:58 +0530

    implement input system package to allow touch inputs

--- Assets/Scenes/Lab/Scripts/InteractableObject.cs
+++ Assets/Scenes/Lab/Scripts/InteractableObject.cs
@@ -2,12 +2,5 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    private void Update()
-    {
-        if (UserInteraction.instance.userInputRay.rayHit)
-        {
 
-            Debug.Log($"Tapped on {UserInteraction.instance.userInputRay.hit.transform.gameObject.name}");
-        }
-    }
 }
--- Assets/Scenes/Lab/Scripts/UserInteraction.cs
+++ Assets/Scenes/Lab/Scripts/UserInteraction.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [System.Serializable]
 public class UserInputRay
@@ -16,22 +17,28 @@ public class UserInteraction : MonoBehaviour
     //ray to be used for mouse point, click, touch
     public UserInputRay userInputRay;
 
+    private void Awake()
+    {
+        InputActions inputActions = new InputActions();
+        inputActions.ActionMap.Enable();
+        inputActions.ActionMap.Select.performed += AttemptObjectSelect;
+    }
+
     private void Start()
     {
         instance = this;
         userInputRay = new();
     }
 
-    private void Update()
+    private void AttemptObjectSelect(InputAction.CallbackContext context)
     {
-        if (Input.GetMouseButtonDown(0))
+        userInputRay.ray = Camera.main.ScreenPointToRay(context.ReadValue<Vector2>());
+        userInputRay.rayHit = Physics.Raycast(userInputRay.ray, out userInputRay.hit, 100);
+
+        if (userInputRay.rayHit)
         {
-            userInputRay.ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            userInputRay.rayHit = Physics.Raycast(userInputRay.ray, out userInputRay.hit, 100);
-        }
-        else
-        {
-            userInputRay.rayHit = false;
+            //currently only interactable objects have colliders
+            Debug.Log($"Tapped on {userInputRay.hit.transform.gameObject.name}");
         }
     }
 }
